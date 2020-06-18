14ba33b5b386ca32576f951bd97e11c6c653bc81
jsp1024 <jsp1024k@naver.com>
2020-06-18 23:07:41 +0900

    디테일 부분 수정

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function About() {
-  return <span>About this page: I built it because I love movies.</span>;
+  return <span>yts 토렌트 사이트에서 영화 다운순위별로 크롤링 하여 소개하는
+    페이지를 만들어 보았습니다.
+  </span>;
 }
 
 export default About;
